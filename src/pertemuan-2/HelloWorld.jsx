cee8fab276a1783b85a5cd026efa613003ec0366
ihsan <ihsan24si@mahasiswa.pcr.ac.id>
2026-03-10 15:25:04 +0700

    First Commit

---
+++ src/pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,54 @@
+export default function HelloWorld(){
+     const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+            <GreetingBinjai/>
+            <QuoteText/>
+            <UserCard
+                nama="Ihsan"
+                nim="12345678"
+                tanggal="2026/03/10"/>
+            <UserCard
+                nama="Afi"
+                nim="12345678"
+                tanggal="2026/03/10"/>
+            <UserCard {...propsUserCard}/>
+        </div>
+    )
+}
+
+function GreetingBinjai(){
+    return (
+        <small>Salam dari Binjai👌 </small>
+    )
+}
+
+function QuoteText() {
+    const text = "Mulutmu Harimaumu";
+    const text2 = "Aku ingin jadi macan";
+    return (
+        <div>
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
+
+function UserCard(props){
+    return (
+        <div className="card">
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+}
