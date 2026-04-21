f59ba0fc2718e98765d3a4931e57395fc88e4772
ihsan <ihsan24si@mahasiswa.pcr.ac.id>
2026-04-21 15:49:21 +0700

    p6 lagi

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import { Route, Routes } from 'react-router-dom';
 import Orders from './pages/Orders';
 import Customers from './pages/Customers';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <div id="main-content" className="flex-1 p-4">
                 <Header />
                 <Routes>
+                    <Route path="*" element={<NotFound />} />
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/orders" element={<Orders />} />
                     <Route path="/customers" element={<Customers />} />
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+    return (
+        <div id="dashboard-container">
+            halaman tidak ditemukan
+        </div>
+    );
+}
