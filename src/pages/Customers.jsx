88e700af7ab82194240e9cfacd77150c81e221a6
ihsan <ihsan24si@mahasiswa.pcr.ac.id>
2026-04-21 15:30:35 +0700

    p6

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,11 @@
+import { FaShoppingCart, FaTruck, FaBan, FaDollarSign } from "react-icons/fa";
+import PageHeader from "../components/PageHeader";
+
+export default function Customers() {
+    return (
+        <div id="dashboard-container">
+            <PageHeader title="Customer" />
+            <p>Ini halaman Customers</p>
+        </div>
+    );
+}
